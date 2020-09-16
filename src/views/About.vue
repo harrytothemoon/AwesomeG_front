c71fee62a2b6b4d6234ecb1b7b0fc03f1a16390d
Harry <harry811016@gmail.com>
2020-09-16 20:51:35 +0800

    新增網站敘述/QA頁面

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,7 +4,59 @@
       <NavTabs />
     </div>
     <div class="mt-5">
-      <h1 class="mb-0 display-4">W e l c o m e !</h1>
+      <div class="card bg-dark text-white border-0">
+        <img
+          class="card-img"
+          src="https://images.unsplash.com/photo-1510936111840-65e151ad71bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1667&q=80"
+          alt="Card image"
+          style="filter: blur(1px)"
+        />
+        <div class="card-img-overlay">
+          <h5 class="card-title display-4">W e l c o m e !</h5>
+          <h6
+            class="card-text mt-4"
+            style="letter-spacing:2px"
+          >This is an online Q & A platform about mathematics, physics and chemistry.</h6>
+          <br />
+          <div class="text-left w-75 mx-auto">
+            <h4 class="card-text mt-4 ml-5" style="letter-spacing:2px">
+              What is
+              <span style="color:#F6B352">Awesome G ?</span>
+            </h4>
+            <h6 class="card-text mt-3 ml-5" style="letter-spacing:1.5px ;line-height: 2">
+              " Awesome G " means awesome logic.
+              <br />We hope to help students who have problems in mathematics, physics and chemistry all over the world through this website.
+              <br />After getting the answer, hope you will say
+              <span
+                style="color:#F6B352"
+              >" Awesome Logic ! ".</span>
+            </h6>
+            <h4 class="card-text mt-5 ml-5" style="letter-spacing:2px">
+              Who is the
+              <span style="color:#F6B352">founder</span> of the website ?
+            </h4>
+            <div class="row mt-2">
+              <div class="col-3 ml-4">
+                <img
+                  class="rounded ml-5 my-4"
+                  src="https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                  alt="Card image cap"
+                  width="150px"
+                  height="150px"
+                />
+              </div>
+              <div class="col-6 ml-3 my-5">
+                <h6 style="letter-spacing:1.5px">
+                  Name :
+                  <span style="color:#F6B352">Harry</span>
+                </h6>
+                <h6 style="letter-spacing:1.5px">Title : Website Founder</h6>
+                <h6 style="letter-spacing:1.5px">Interests : Ask the great question.</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- src/views/Qa.vue
+++ src/views/Qa.vue
@@ -3,7 +3,39 @@
     <div class="d-flex justify-content-center mt-2">
       <NavTabs />
     </div>
-    <h1 class="mt-5 text-center">Q & A</h1>
+    <div class="d-flex flex-column justify-content-center align-items-center">
+      <ul class="mt-5 nav nav-tabs w-75">
+        <li class="nav-item w-50">
+          <a class="nav-link active text-center" data-toggle="tab" href="#student">
+            <h4>About the Student</h4>
+          </a>
+        </li>
+        <li class="nav-item w-50">
+          <a class="nav-link text-center" data-toggle="tab" href="#teacher">
+            <h4>About the Teacher</h4>
+          </a>
+        </li>
+      </ul>
+      <div class="rounded pb-5 pt-4" style="background-color:#202020 ;width:1000px">
+        <div id="myTabContent" class="tab-content text-light w-75 rounded-bottom mx-auto">
+          <div class="tab-pane fade active show text-center" id="student">
+            <h2 class="mt-2 text-left" style="color:#F6B352">Q1: How do I start asking questions ?</h2>
+            <h5
+              class="my-4 text-left"
+            >If you have an available balance in your account, please follow the instructions below.</h5>
+            <img class="mb-5" src="../../public/Q&A2.png" alt="Q&A" width="750px" />
+            <h2 class="mt-3 text-left" style="color:#F6B352">Q2: How do I buy credits ?</h2>
+            <h5 class="my-4 text-left">Please follow the instructions below.</h5>
+            <img src="../../public/Q&A1.png" alt="Q&A" width="750px" />
+          </div>
+          <div class="tab-pane fade text-center" id="teacher">
+            <h2 class="mt-2 text-left" style="color:#F6B352">Q1: How do I answer the question ?</h2>
+            <h5 class="my-4 text-left">Please follow the instructions below.</h5>
+            <img src="../../public/Q&A3.png" alt="Q&A" width="750px" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
